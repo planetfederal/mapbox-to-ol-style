5ef3f998407c281ade60a7f0f6b5afc9f8a50902
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-01-23 21:17:48 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,42 +1,14 @@
-import expect from 'expect.js';
-import olms from '../';
-import VectorTileLayer from 'ol/layer/vectortile';
-import VectorTileSource from 'ol/source/vectortile';
-import projCommon from 'ol/proj/common';
-import tilegrid from 'ol/tilegrid';
-import brightV9 from '../node_modules/mapbox-gl-styles/styles/bright-v9.json';
+import should from 'should/as-function';
+import getStyleFunction from '../';
+import states from '../example/data/states.json';
 
-describe('ol-mapbox-gl-style', function() {
-
-  projCommon.add();
-
-  var layer;
-  beforeEach(function() {
-    layer = new VectorTileLayer({
-      source: new VectorTileSource({
-        tileGrid: tilegrid.createXYZ({tileSize: 512, maxZoom: 22})
-      })
-    });
-  });
-
+describe('mapbox-to-ol-style', function() {
 
   describe('getStyleFunction', function() {
-    it('creates a style function and calls onChange', function(done) {
-      var resolutions = layer.getSource().getTileGrid().getResolutions();
-      var style = olms.getStyleFunction(brightV9, 'mapbox', resolutions, function() {
-        done();
-        // Callback will be called multiple times, but call done8) only once
-        done = function() {};
-      });
-      expect(typeof style).to.be('function');
+    it('creates a style function', function() {
+      var style = getStyleFunction(states, 'states');
+      should(style).be.a.Function;
     });
   });
 
-  describe('applyStyle', function() {
-    it('applies a style function to a layer and resolves promise', function(done) {
-      olms.applyStyle(layer, brightV9, 'mapbox').then(function() {
-        done();
-      });
-    });
-  });
 });
