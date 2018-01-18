88944f5b4c96776abeab7c12b9408dc5b53d877e
Andreas Hocevar <andreas.hocevar@gmail.com>
2018-01-18 14:34:48 +0100

    Make resolutions compatible with mapbox-gl-js

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ function fromTemplate(text, properties) {
 export default function(olLayer, glStyle, source, resolutions, spriteData, spriteImageUrl, fonts) {
   if (!resolutions) {
     resolutions = [];
-    for (var res = 156543.03392804097; resolutions.length < 22; res /= 2) {
+    for (var res = 78271.51696402048; resolutions.length < 21; res /= 2) {
       resolutions.push(res);
     }
   }
